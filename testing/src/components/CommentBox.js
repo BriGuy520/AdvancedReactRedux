d03083770f3e2d7cbe353f4cd467408bd563804b
Brian Falasz <brianfalasz@gmail.com>
2018-12-19 18:32:47 -0600

    added an api call to jsonplaceholder made with axios. Also, added a few tests.

--- testing/src/components/CommentBox.js
+++ testing/src/components/CommentBox.js
@@ -1,4 +1,6 @@
 import React, { Component } from 'react';
+import { connect } from 'react-redux';
+import { saveComment, fetchComments } from 'actions';
 
 class CommentBox extends Component {
 
@@ -12,21 +14,26 @@ class CommentBox extends Component {
   handleSubmit = (event) => {
     event.preventDefault();
 
+    this.props.saveComment(this.state.comment);
     // Call an action creator and save the comment
     this.setState({ comment: '' });
   }
 
   render(){
     return (
-      <form onSubmit={this.handleSubmit}> 
-        <h4>Add a Comment</h4>
-        <textarea value={this.state.comment} onChange={this.handleChange}/>
-        <div>
-          <button>Submit Comment</button>
-        </div>
-      </form>
+      <div>
+         <form onSubmit={this.handleSubmit}>
+          <h4>Add a Comment</h4>
+          <textarea value={this.state.comment} onChange={this.handleChange}/>
+          <div>
+            <button>Submit Comment</button>
+          </div>
+        </form>
+        <button onClick={this.props.fetchComments}>Show Comments</button>
+      </div>
+     
     )
   }
 }
 
-export default CommentBox;
+export default connect(null, { saveComment, fetchComments })(CommentBox);
