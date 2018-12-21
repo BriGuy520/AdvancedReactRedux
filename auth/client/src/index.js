1661c6e78f109df353c2ed394541ce4d0e4940e8
Brian Falasz <brianfalasz@gmail.com>
2018-12-21 11:04:21 -0600

    added a reducers file for authorization. Also, added react-redux and redux to app component to wire up reducers

--- auth/client/src/index.js
+++ auth/client/src/index.js
@@ -1,14 +1,23 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import { BrowserRouter, Route } from 'react-router-dom';
+import { Provider } from 'react-redux';
+import { createStore } from 'redux'; 
 
+import reducers from './reducers';
 import App from './components/App';
 import Welcome from './components/Welcome';
+import SignUp from './components/auth/SignUp';
+
+const store = createStore(reducers, {});
 
 ReactDOM.render(
-<BrowserRouter>
-  <App>
-    <Route path="/" exact component={Welcome} />
-  </App> 
-</BrowserRouter>, 
+<Provider store={store}>
+  <BrowserRouter>
+    <App>
+      <Route path="/" exact component={Welcome} />
+      <Route path="/signup" component={SignUp} />
+    </App> 
+  </BrowserRouter>
+</Provider>, 
 document.querySelector('#root'));
